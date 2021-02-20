dbf601c4ba378c8871e6098ad2f4ccec5afdb69b
Deepak101094 <deepakpandey101094@gmail.com>
2021-02-20 14:21:00 +0530

    login page desiging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 //? components
 import HomeScreen from "./components/screen/homeScreen";
+import LoginScreen from "./components/screen/loginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          {!user ? (
+            <LoginScreen />
+          ) : (
+            <Route exact={true} path="/">
+              <HomeScreen />
+            </Route>
+          )}
+        </Switch>
+      </Router>
     </div>
   );
 }
